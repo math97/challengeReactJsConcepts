f97146f75caaf1cf49585b0dc1d76a5c1abb3e9b
Matheus Alves Albuquerque <math.albuquerque@outlook.com>
2022-11-14 20:41:06 +0000

    add new tasks

---
+++ src/components/Task.tsx
@@ -0,0 +1,29 @@
+import styles from './Task.module.css';
+
+import { Trash,Circle, CheckCircle} from 'phosphor-react'
+
+
+interface ITaskProps {
+  task : ITask
+}
+
+export interface ITask {
+  id : string;
+  content: string;
+  isDone: boolean;
+}
+
+export function Task({task}:ITaskProps){
+  console.log(`aquiii`,task);
+  return (
+    <div className={styles.task}>
+      <button className={task.isDone ? styles.done : styles.notDone} >
+        {task.isDone ? <CheckCircle size={24} /> : <Circle size={24} /> }
+      </button>
+      <span className={styles.content}>{task.content}</span>
+      <button>
+        <Trash size={24} />
+      </button>
+    </div>
+  )
+}
--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,29 +1,59 @@
 import clipBoard from '../assets/clipboard.svg'
 
+import {Task,ITask} from './Task'
+
 import styles from './TaskList.module.css';
 
-interface TaskProps {
-  tasks : Task[]
-}
+// interface TaskProps {
+//   tasks : Task[]
+// }
 
-interface Task {
-  id : string;
-  title: string;
-  content: string
-}
+// interface Task {
+//   id : string;
+//   content: string;
+//   isDone: boolean;
+// }
 
 export function TaskList(){
-  const tasks = []
+  const tasks:ITask[] = [
+    {
+      id:'1',
+      content:"hsagshgahashsashsagshgahashsashsagshgasasssasasaasasasaasassasasassasasasasassaasasasasasasassasas",
+      isDone:false,
+    },
+    {
+      id:'2',
+      content:"testando",
+      isDone:false,
+    },
+    {
+      id:'3',
+      content:"bora birl",
+      isDone:true,
+    }
+  ]
   
-  if(tasks.length === 0) return (
-    <div className={styles.taskListEmpty}>
-        <img src={clipBoard} alt="clipboard image when exist no tasks" />
-        <strong>Você ainda não tem tarefas cadastradas</strong>
-        <span>Crie tarefas e organize seus itens a fazer</span>
-    </div>
-  )
+  if(tasks.length === 0) 
+  {
+    return (
+      <div className={styles.taskListEmpty}>
+          <img src={clipBoard} alt="clipboard image when exist no tasks" />
+          <strong>Você ainda não tem tarefas cadastradas</strong>
+          <span>Crie tarefas e organize seus itens a fazer</span>
+      </div>
+    )
+  }else{
+    return (
+      <div className={styles.taskList}>
+        {
+          tasks.map(task =>{
+            return (
+              <Task task={task} />
+            )
+          })  
+        }
+      </div>
+    )
+  }
   
-  return (
-    <p>tem task</p>
-  )
 }
